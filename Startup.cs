0ab65f54dfbcaa82e09f86294fb27c5b487bd6b5
Masui Masanori <masanori_msl@outlook.com>
2020-08-26 01:21:10 +0900

    init

---
+++ Startup.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace FileAccessSample
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // 属性ベースのルーティングを有効にする.
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
